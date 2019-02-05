a14ea38e634ab99befab06c6cdf7d209a25a3c1b
Antheny Wells <anthenywells@Go-away.local>
2019-02-05 08:52:59 -0500

    jQuery updates

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
 "use strict";
 
 
-
+$(document).ready(() => {
 
 //opens form, changes tables class
-$(".available").on("click", function(){
-  let selectedTable = this.id;
+$(".available").on("click", function(e){
+  let selectedTable = $(e.target)
   console.log(selectedTable)
   $("#form").css("display", "flex");
   $("#tabNum").text(`${$(this).get(0).id}`);
-  $("#submit").on("click", function(){
-    $(selectedTable).addClass("reserved");
-  });
+});
+
+//submit closes form and reserves table
+$("#submit").on("click", function(){
+  $(selectedTable).addClass("reserved");
+  $("#form").css("display", "none")
 });
 
 //X closes form
 $(".close").on("click", function(){
   $("#form").css("display", "none")
 })
-// submit closes form
-$("#submit").on("click", function(){
-  $("#form").css("display", "none")
-})
+
 //clicking outside of the form closes the form
-$(window).on("click", function(e){
-  if(e.target.id == 'form'){
-      $('#form').css("display", "none");
-  }
+// $(window).on("click", function(e){
+//   if(e.target.id == 'form'){
+//       $('#form').css("display", "none");
+//   }
+// });
+
 });
