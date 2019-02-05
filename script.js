6e446cf9e601a6fe6a9250b2a27146316b40b877
Antheny Wells <anthenywells@Go-away.local>
2019-02-05 18:49:40 -0500

    Added tooltip and form transition

--- script.js
+++ script.js
@@ -3,30 +3,41 @@
 
 $(document).ready(() => {
 
+let selectedTable;
+
 //opens form, changes tables class
-$(".available").on("click", function(e){
-  let selectedTable = $(e.target)
-  console.log(selectedTable)
-  $("#form").css("display", "flex");
-  $("#tabNum").text(`${$(this).get(0).id}`);
+$(document).on("click", ".available", (e) => {
+  selectedTable = $(e.target)
+  $("#form").fadeIn(250).css("display", "flex");
+  $("#tabNum").text(`${selectedTable.get(0).id}`);
 });
 
 //submit closes form and reserves table
-$("#submit").on("click", function(){
-  $(selectedTable).addClass("reserved");
-  $("#form").css("display", "none")
+$(document).on("click", "#submit", () => {
+  $(selectedTable)
+  .removeClass("available").addClass("reserved")
+  .attr("name", $("#name").val())
+  .attr("guests", $("#howMany").val());
+  $("#form").fadeOut(150)
 });
-
 //X closes form
-$(".close").on("click", function(){
-  $("#form").css("display", "none")
+$(document).on("click", ".close", () => {
+  $("#form").fadeOut(150)
 })
-
-//clicking outside of the form closes the form
-// $(window).on("click", function(e){
-//   if(e.target.id == 'form'){
-//       $('#form').css("display", "none");
-//   }
-// });
-
+//displays reserved table attributes
+$(document).on("mouseenter", ".reserved", (event) => {
+  if ($(event.target).attr("name") && $(event.target).attr("guests")) {
+    $(event.target).append(`
+    <section class="tooltip">
+      Name: ${$(event.target).attr("name")}<br>
+      Guests: ${$(event.target).attr("guests")}
+    </section>
+    `);
+   } 
 });
+//hides reserved table attributes
+$(document).on("mouseleave", ".reserved", (event) => {
+  $(".tooltip").remove();
+});
+});
+
